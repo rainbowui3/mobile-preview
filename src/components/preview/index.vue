8a5c2b3fd6a474d6608c907761eec4b3da01e1ae
张海斌 <tony@zhanghaibindeMacBook-Pro.local>
2018-03-22 21:56:29 +0800

    add

---
+++ src/components/preview/index.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+        <form-preview :header-label="title" :header-value="value" :body-items="data" :footer-buttons="buttons" :name="param"></form-preview>
+  </div>
+</template>
+
+<script>
+const FormPreview =  require("vux/src/components/form-preview/index.vue");
+export default {
+  props: {
+    title: String,
+    value: String,
+    data:Array,
+    buttons:Array,
+    param:Object
+  },
+  components: {
+    FormPreview
+  }
+}
+</script>
+<style>
+.weui-form-preview__btn_primary {
+   color: #4D93E4!important;
+}
+</style>
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Preview from './components/preview/index.vue';
+
+export {
+    Preview,
+};
